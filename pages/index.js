9f0f4f857595614ebc9067c4ed1c067f1b3f3583
mark9joe <136325567+mark9joe@users.noreply.github.com>
2025-06-23 01:36:12 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Welcome to RespireWork 🚀</h1>
+      <p>This is the homepage. Dynamic pages will load below this.</p>
+    </main>
+  );
+}
