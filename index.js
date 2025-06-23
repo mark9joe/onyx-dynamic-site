76f86bf8d9af38b4e963629cf8320571174bb6a7
mark9joe <136325567+mark9joe@users.noreply.github.com>
2025-06-23 13:27:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Welcome to RespireWork</h1>
+      <p>This is your homepage. Dynamic SEO pages are now enabled using ISR.</p>
+    </main>
+  );
+}
