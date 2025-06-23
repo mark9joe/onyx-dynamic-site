f106a1a6ede33afd60702d8cdd97ce13cc098515
mark9joe <136325567+mark9joe@users.noreply.github.com>
2025-06-23 23:21:37 +0530

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -3,7 +3,8 @@ import { useRouter } from 'next/router';
 
 export default function SlugPage() {
   const router = useRouter();
-  const { slug } = router.query;
+  return <h1>Slug: {router.query.slug}</h1>;
+};
 
   return (
     <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
