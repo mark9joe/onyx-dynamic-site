af2287ed4f025850e47ec3d568e9c803cb25b7e6
mark9joe <136325567+mark9joe@users.noreply.github.com>
2025-06-25 22:03:52 +0530

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,30 +1,16 @@
-// pages/[slug].js
-import { useRouter } from 'next/router';
-import Head from 'next/head';
-
-export default function SlugPage() {
-  const router = useRouter();
-  const { slug } = router.query;
-
-  if (!slug) return <div>Loading...</div>;
-
+export default function Page({ slug }) {
   return (
-    <>
-      <Head>
-        <title>{slug} | Onyx Dynamic Site</title>
-        <meta name="description" content={`Details about ${slug}`} />
-        <meta property="og:title" content={`${slug} | Onyx Dynamic Site`} />
-        <meta property="og:description" content={`Details about ${slug}`} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={`https://www.respirework.com/${slug}`} />
-        <meta name="twitter:card" content="summary" />
-      </Head>
-      <h1>Redirecting...</h1>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.location.href = "https://www.respirework.com";`
-        }}
-      />
-    </>
+    <div>
+      <h1>Page Live: {slug}</h1>
+      <p>This dynamic route is working!</p>
+    </div>
   );
+}
+
+export async function getServerSideProps({ params }) {
+  return {
+    props: {
+      slug: params.slug || 'default',
+    },
+  };
 }
